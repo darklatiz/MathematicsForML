8cf93a05fb843a1eec5283b8d91f072f2fe4af35
Spike Storm <software.j2ee@gmail.com>
2020-04-28 09:50:02 -0700

    Comparison between methods to calculate dot product

--- intro/udemy/code_challenge_dot_product_matrix.py
+++ intro/udemy/code_challenge_dot_product_matrix.py
@@ -20,10 +20,19 @@ numRows = np.size(M1, 0)
 # cols
 numCols = np.size(M1, 1)
 
+r1 = np.zeros(numCols)
+r2 = np.zeros(numCols)
+
 for i in range(numCols):
     dot_prodcut = dot_product_for_loop(M1[:, i], M2[:, i])
+    r1[i] = dot_prodcut
     print("Dot LOOP Method product for columns  {0} : {1}".format(i, dot_prodcut))
 
     dot_prodict_numpy = np.dot(M1[:, i], M2[:, i])
+    r2[i] = dot_prodict_numpy
     print("Dot NUMPY Method product for columns {0} : {1}".format(i, dot_prodict_numpy))
 
+print("DEtecting different values from method 1 and method 2 \n\n")
+for i in range(numCols):
+    if r1[i] != r2[i]:
+        print("{0} is not equal to {1} the difference is {2}".format(r1[i], r2[i], r1[i]-r2[i]))
